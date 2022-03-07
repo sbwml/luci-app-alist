e2fd8f52adde60d74e8dd73374252c7cca453e4b
sbwml <admin@cooluc.com>
2022-03-07 17:55:31 +0800

    luci: enable SSL protocol support & format code

--- luci-app-alist/luasrc/model/cbi/alist.lua
+++ luci-app-alist/luasrc/model/cbi/alist.lua
@@ -4,19 +4,44 @@ m = Map("alist", translate("Alist"), translate("Alist is an file list program th
 
 m:section(SimpleSection).template  = "alist_status"
 
-s=m:section(TypedSection, "alist", translate("Global settings"))
-s.addremove=false
-s.anonymous=true
+s = m:section(TypedSection, "alist", translate("Global settings"))
+s.addremove = false
+s.anonymous = true
 
-s:option(Flag, "enabled", translate("Enable")).rmempty=false
-s:option(Value, "port", translate("Port")).rmempty=false
+o = s:option(Flag, "enabled", translate("Enable"))
+o.rmempty = false
 
-s=m:section(TypedSection, "alist", translate("Cache settings"))
-s.addremove=false
-s.anonymous=true
+o = s:option(Value, "port", translate("Port"))
+o.datatype = "and(port,min(1))"
+o.rmempty = false
 
-s:option(Value, "expiration", translate("Cache invalidation time (unit: minutes)")).rmempty=false
-s:option(Value, "cleanup_interval", translate("Clear the invalidation cache interval")).rmempty=false
-s:option(Value, "temp_dir", translate("Temp directory")).rmempty=false
+o = s:option(Flag, "ssl", translate("Enable SSL"))
+o.rmempty=false
+
+o = s:option(Value,"ssl_cert", translate("SSL cert"), translate("SSL certificate file path"))
+o:depends("ssl", "1")
+o.datatype = "string"
+o.rmempty = true
+
+o = s:option(Value,"ssl_key", translate("SSL key"), translate("SSL key file path"))
+o:depends("ssl", "1")
+o.datatype = "string"
+o.rmempty = true
+
+s = m:section(TypedSection, "alist", translate("Cache settings"))
+s.addremove = false
+s.anonymous = true
+
+o = s:option(Value, "expiration", translate("Cache invalidation time (unit: minutes)"))
+o.datatype = "and(uinteger,min(1))"
+o.rmempty = false
+
+o = s:option(Value, "cleanup_interval", translate("Clear the invalidation cache interval (unit: minutes)"))
+o.datatype = "and(uinteger,min(1))"
+o.rmempty = false
+
+o = s:option(Value, "temp_dir", translate("Temp directory"))
+o.datatype = "string"
+o.rmempty = false
 
 return m
