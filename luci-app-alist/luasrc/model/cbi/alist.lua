caceafb2ba285dbb0b9c5902c875a0c21b29403b
sbwml <admin@cooluc.com>
2022-08-03 03:30:27 +0800

    luci-app-alist: add usage documentation link

--- luci-app-alist/luasrc/model/cbi/alist.lua
+++ luci-app-alist/luasrc/model/cbi/alist.lua
@@ -3,9 +3,9 @@ local sys  = require "luci.sys"
 
 if sys.call("pidof alist >/dev/null") == 0 then
 	local password = sys.exec("/usr/bin/alist --conf /etc/alist/config.json -password | awk '{print $3}'")
-	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. " " .. translate("manage password:") .. "<font color=\"green\">" .. password .. "</font>")
+	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. " " .. translate("manage password:") .. "<font color=\"green\">" .. password .. "</font>" .. "<br/>" .. [[<a href="https://alist-doc.nn.ci/docs/driver/native" target="_blank">]] .. translate("User Manual") .. [[</a>]])
 else
-	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage."))
+	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. "<br/>" .. [[<a href="https://alist-doc.nn.ci/docs/driver/native" target="_blank">]] .. translate("User Manual") .. [[</a>]])
 end
 
 m:section(SimpleSection).template  = "alist_status"
@@ -46,7 +46,7 @@ o = s:option(Value, "cleanup_interval", translate("Clear the invalidation cache 
 o.datatype = "and(uinteger,min(1))"
 o.rmempty = false
 
-o = s:option(Value, "temp_dir", translate("Temp directory"))
+o = s:option(Value, "temp_dir", translate("Cache directory"))
 o.datatype = "string"
 o.rmempty = false
 
