4d00052697ad17c0a59602cc89a5dd868c77575e
sbwml <984419930@qq.com>
2022-09-09 19:22:50 +0800

    luci-app-alist: update for v3

--- luci-app-alist/luasrc/model/cbi/alist.lua
+++ luci-app-alist/luasrc/model/cbi/alist.lua
@@ -2,10 +2,10 @@ local m, s
 local sys  = require "luci.sys"
 
 if sys.call("pidof alist >/dev/null") == 0 then
-	local password = sys.exec("/usr/bin/alist --conf /etc/alist/config.json -password | awk '{print $3}'")
-	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. " " .. translate("manage password:") .. "<font color=\"green\">" .. password .. "</font>" .. "<br/>" .. [[<a href="https://alist-doc.nn.ci/docs/driver/native" target="_blank">]] .. translate("User Manual") .. [[</a>]])
+	local password = sys.exec("/usr/bin/alist --conf /etc/alist/config.json -password |& tail -1 | awk -F': ' '{print $2}'")
+	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. " " .. translate("manage password:") .. "<font color=\"green\">" .. password .. "</font>" .. "<br/>" .. [[<a href="https://alist.nn.ci/guide/drivers/local.html" target="_blank">]] .. translate("User Manual") .. [[</a>]])
 else
-	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. "<br/>" .. [[<a href="https://alist-doc.nn.ci/docs/driver/native" target="_blank">]] .. translate("User Manual") .. [[</a>]])
+	m = Map("alist", translate("Alist"), translate("A file list program that supports multiple storage.") .. "<br/>" .. [[<a href="https://alist.nn.ci/guide/drivers/local.html" target="_blank">]] .. translate("User Manual") .. [[</a>]])
 end
 
 m:section(SimpleSection).template  = "alist_status"
@@ -21,6 +21,11 @@ o = s:option(Value, "port", translate("Port"))
 o.datatype = "and(port,min(1))"
 o.rmempty = false
 
+o = s:option(Value, "temp_dir", translate("Cache directory"))
+o.datatype = "string"
+o.default = "/tmp/alist"
+o.rmempty = false
+
 o = s:option(Flag, "ssl", translate("Enable SSL"))
 o.rmempty=false
 
@@ -33,21 +38,5 @@ o = s:option(Value,"ssl_key", translate("SSL key"), translate("SSL key file path
 o:depends("ssl", "1")
 o.datatype = "string"
 o.rmempty = true
-
-s = m:section(TypedSection, "alist", translate("Cache settings"))
-s.addremove = false
-s.anonymous = true
-
-o = s:option(Value, "expiration", translate("Cache invalidation time (unit: minutes)"))
-o.datatype = "and(uinteger,min(1))"
-o.rmempty = false
-
-o = s:option(Value, "cleanup_interval", translate("Clear the invalidation cache interval (unit: minutes)"))
-o.datatype = "and(uinteger,min(1))"
-o.rmempty = false
-
-o = s:option(Value, "temp_dir", translate("Cache directory"))
-o.datatype = "string"
-o.rmempty = false
 
 return m
